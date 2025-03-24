33bf37c9bdb4e1b4333972493cbbfc7e04bf847e
Xander <2695418206@qq.com>
2025-03-24 17:15:28 +0800

    paylabs审核

--- others/Investment/THS/AutoTrade/scripts/策略/策略_今天调仓.py
+++ others/Investment/THS/AutoTrade/scripts/策略/策略_今天调仓.py
@@ -20,10 +20,7 @@ from others.Investment.THS.AutoTrade.utils.determine_market import determine_mar
 from others.Investment.THS.AutoTrade.utils.logger import setup_logger
 # from utils.logger import setup_logger
 from others.Investment.THS.AutoTrade.utils.notification import send_notification, send_email
-
-# from utils.notification import send_notification
-
-# from utils.notification import
+from others.Investment.THS.AutoTrade.utils.excel_handler import save_to_excel, clear_sheet
 
 logger = setup_logger(STRATEGY_TODAY_ADJUSTMENT_LOG_FILE)
 
@@ -65,42 +62,6 @@ def get_latest_position_and_trade(strategy_id):
             return latest_trade_info, trade_date
         else:
             return [], 'N/A'
-
-def save_to_excel(df, filename, sheet_name, index=False):
-    """将DataFrame保存到Excel文件中"""
-    try:
-        # 检查文件是否存在
-        try:
-            with pd.ExcelFile(filename) as _:
-                # 文件存在，追加模式
-                with pd.ExcelWriter(filename, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
-                    df.to_excel(writer, sheet_name=sheet_name, index=index)
-        except FileNotFoundError:
-            # 文件不存在，创建新文件
-            with pd.ExcelWriter(filename, engine='openpyxl') as writer:
-                df.to_excel(writer, sheet_name=sheet_name, index=index)
-            pprint(f"成功保存数据到文件: {filename}, 表名称: {sheet_name}")
-    except Exception as e:
-        logger.error(f"保存数据到文件失败: {e}")
-
-def clear_sheet(filename, sheet_name):
-    """清空指定Excel文件中的指定表格"""
-    try:
-        # 检查文件是否存在
-        try:
-            wb = openpyxl.load_workbook(filename)
-            if sheet_name in wb.sheetnames:
-                ws = wb[sheet_name]
-                # 删除所有行
-                ws.delete_rows(1, ws.max_row)
-                wb.save(filename)
-                pprint(f"成功清空表格: {sheet_name} 文件: {filename}")
-            else:
-                logger.warning(f"表格 {sheet_name} 不存在于文件: {filename}")
-        except FileNotFoundError:
-            logger.warning(f"文件 {filename} 不存在，无需清空")
-    except Exception as e:
-        logger.error(f"清空表格失败: {e}")
 
 async def check_new_data(existing_df, today_trade_df, sheet_name):
     """优化后的数据合并函数
--- others/Investment/THS/AutoTrade/zothers/tes.py
+++ others/Investment/THS/AutoTrade/zothers/tes.py
@@ -1,15 +1,80 @@
+from pprint import pprint
 
-#访问百度网站
-import requests
+exists_data = {
+    "记录": [
+        {
+            "组合名称": "超短稳定",
+            "代码": "600169",
+            "股票名称": "太原重工",
+            "市场": "沪深A股",
+            "操作": "卖出",
+            "最新价": 2.76,
+            "当前比例%": 5.19,
+            "新比例%": 0
+        },
+        {
+            "组合名称": "超短稳定",
+            "代码": "002445",
+            "股票名称": "中南文化",
+            "市场": "沪深A股",
+            "操作": "买入",
+            "最新价": 2.52,
+            "当前比例%": 0,
+            "新比例%": 20
+        }
+    ]
+}
 
-print(requests.__file__)
+compare_data = {
+    "记录": [
+        {
+            "组合名称": "超短稳定",
+            "代码": "600169",
+            "股票名称": "太原重工",
+            "市场": "沪深A股",
+            "操作": "卖出",
+            "最新价": 2.76,
+            "当前比例%": 5.19,
+            "新比例%": 0
+        },
+        {
+            "组合名称": "超短稳定",
+            "代码": "002445",
+            "股票名称":"中南文化",
+            "市场": "沪深A股",
+            "操作": "买入",
+            "最新价": 2.52,
+            "当前比例%": 0,
+            "新比例%": 20
+        }
+        ,
+        {
+            "组合名称": "每天进步一点点",
+            "代码": "002445",
+            "股票名称": "中南文化",
+            "市场": "沪深A股",
+            "操作": "买入",
+            "最新价": 2.52,
+            "当前比例%": 0,
+            "新比例%": 20
+        }
+    ]
+}
 
-#
-# def get_html():
-#     url = 'https://www.baidu.com'
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-#     }
-#     response = requests.get(url=url,headers=headers)
-#     print(response.headers)
-# get_html()
+#对比两个data每一个列表的每一个字段，找出compare里和exists里不同那一条记录，并通知
+def compare_data_and_notify(exists_data, compare_data):
+    for exists_record in exists_data:
+        for compare_record in compare_data:
+            if exists_record['组合名称'] == compare_record['组合名称']:
+                if exists_record['代码'] != compare_record['代码']:
+                    print(f"{exists_record['组合名称']} 的代码有变化，从 {exists_record['代码']} 变为 {compare_record['代码']}")
+                else:
+                    print()
+            # if exists_record['代码'] == compare_record['代码']:
+            #     if exists_record['新比例%'] != compare_record['新比例%']:
+            #         print(f"{exists_record['代码']} 的新比例有变化，从 {exists_record['新比例%']} 变为 {compare_record['新比例%']}")
+            #     else:
+            #         print()
+compare_data_and_notify(exists_data, compare_data)
+# # 控制台输出示例
+# pprint()
